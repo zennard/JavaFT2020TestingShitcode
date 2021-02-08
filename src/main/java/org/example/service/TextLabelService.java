807ef36ab3c1dedb8f154fbdbf35f4d3b2b37af5
Zennard <podlesniukbogdan@gmail.com>
2021-02-08 09:01:46 +0200

    - Refactored TextLabelService

--- src/main/java/org/example/service/TextLabelService.java
+++ src/main/java/org/example/service/TextLabelService.java
@@ -3,16 +3,19 @@ package org.example.service;
 import org.apache.commons.lang3.StringUtils;
 import org.example.dao.TextLabelDao;
 import org.example.domain.Site;
+import org.example.domain.TextLabel;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TextLabelService {
 
     protected static final String EMPTY_MESSAGE_PLACEHOLDER = "{EMPTY_MESSAGE}";
+    private static final String EMPTY_STRING = "";
 
     private SiteService siteService;
 
@@ -20,34 +23,51 @@ public class TextLabelService {
 
     public Optional<String> getMessage(String key, Locale locale) {
         Site site = siteService.getCurrentSite();
-        return getMessageWithFallbackToNoSite(key, locale, site);
+        return getSiteSpecificMessage(key, locale, site);
     }
 
-    private Optional<String> getMessageWithFallbackToNoSite(String key, Locale locale, Site site) {
-        return Stream.of(site, null)
-                .distinct()
-                .map(s -> getSiteSpecificMessage(key, locale, s))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(this::replaceEmptyMessagePlaceholder)
-                .findFirst();
-    }
-
+    /*
+    * returns Optional of label value with this locale, key and for specific site,
+    * if label value is empty placeholder - changes it on empty string
+    * */
     private Optional<String> getSiteSpecificMessage(String key, Locale locale, Site site) {
         Map<String, Object> params = new HashMap<>();
         params.put("key", key);
         params.put("site", site);
-        return textLabelDao.find(params, locale).stream()
-                .map(textLabelModel -> textLabelModel.getValue(locale))
-                .filter(StringUtils::isNotEmpty)
-                .findAny();
+
+        //from many duplicate labels for thi site we are getting exactly one???
+        List<TextLabel> textLabels = textLabelDao.find(params, locale);
+
+        return getSpecificMessage(textLabels, locale);
     }
 
-    private String replaceEmptyMessagePlaceholder(String string) {
-        if (EMPTY_MESSAGE_PLACEHOLDER.equals(string)) {
-            return "";
+    private Optional<String> getSpecificMessage(List<TextLabel> textLabels, Locale locale) {
+        List<String> localizedMessages = textLabels.stream()
+                .map(label -> label.getValue(locale))
+                .collect(Collectors.toList());
+
+        List<String> filteredMessages = filterMessages(localizedMessages, StringUtils::isNotEmpty);
+        List<String> transformedMessages = transformMessages(filteredMessages, this::transformEmptyMessage);
+
+        return transformedMessages.stream().findAny();
+    }
+
+    private List<String> transformMessages(List<String> messages, UnaryOperator<String> function) {
+        return messages.stream()
+                .map(function)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> filterMessages(List<String> messages, Predicate<? super String> predicate) {
+        return messages.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+    private String transformEmptyMessage(String message) {
+        boolean isPlaceholderEmpty = EMPTY_MESSAGE_PLACEHOLDER.equals(message);
+        if (isPlaceholderEmpty) {
+            return EMPTY_STRING;
         }
-        return string;
+        return message;
     }
 
     public void setSiteService(SiteService siteService) {
